0d285b5a8bd366fddbc1b00f5e56e15518b86270
Yummy_Mommy <68540013+Y0ungM0rris@users.noreply.github.com>
2024-05-16 06:30:32 +0300

    Update test0.py

--- test0.py
+++ test0.py
@@ -3,29 +3,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Получаем в переменную browser указатель на браузер
 browser = webdriver.Chrome()
 
-# Переходим на страницу, на которой находится форма для авторизации
 browser.get('https://auth.edimdoma.ru/login?service=https%3A%2F%2Fwww.edimdoma.ru%2Fusers%2Fservice')
 
-# заполняем поле логин, привязываемся к элементу через его имя
-username = browser.find_element(by=By.NAME, value='username')
-username.send_keys('dmorev17@mail.ru')
-
-# заполняем поле пароля, привязываемся к элементу через его id
-password = browser.find_element(by=By.NAME, value='password')
-password.send_keys('qwerty')
-
-# Получаем указатель на кнопку "Вход", привязываемся к элементу через его css_selector
-button = browser.find_element(by=By.NAME, value='button')
-# Нажимаем на кнопку входа
-button.click()
-# Проверка результата
-
 try:
+    username = browser.find_element(by=By.NAME, value='username')
+    username.send_keys('dmorev17@mail.ru')
+    
+    password = browser.find_element(by=By.NAME, value='password')
+    password.send_keys('qwerty')
+    
+    button = browser.find_element(by=By.NAME, value='button')
+    button.click()
     print('The test was completed successfully')
-except Exception as err:
-    print('The test was failled')
 
-# Закрываем браузер
+except Exception as err:
+    print('The test was failed')
+    print(f'Error: {err}')
+
+browser.quit()
